9a25ebf90502e6e407847418ee00855e223bbe7b
Chhel Singh <chhelsingh16@gmail.com>
2020-08-11 11:31:52 +0530

    Added Level Generation and Destroy Effect

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using static UnityEngine.ParticleSystem;
+
+public class Brick : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    public int HitPoints = 1;
+    public ParticleSystem DestroyEffect;
+    public static event Action<Brick> OnBrickDestruction;
+
+    private void Awake() 
+    {
+        this.sr = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        ApplyCollisionLogic(ball);
+    }
+
+    private void ApplyCollisionLogic(Ball ball)
+    {
+        this.HitPoints--;
+
+        if(this.HitPoints <= 0)
+        {
+            OnBrickDestruction?.Invoke(this);
+            //Spawn Destroy Efect
+            SpawanDestroyEffect();
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            this.sr.sprite = BrickManager.Instance.Sprites[this.HitPoints -1];
+        }        
+    }
+
+    private void SpawanDestroyEffect()
+    {
+        Vector3 brickPos = gameObject.transform.position;
+        Vector3 spawnPosition = new Vector3(brickPos.x, brickPos.y, brickPos.z - 0.2f);
+        GameObject effect = Instantiate(DestroyEffect.gameObject, spawnPosition, Quaternion.identity);
+
+        MainModule mm = effect.GetComponent<ParticleSystem>().main;
+        mm.startColor = this.sr.color;
+        Destroy(effect, DestroyEffect.main.startLifetime.constant);
+    }
+
+    public void Init(Transform containerTransform, Sprite sprite, Color color, int hitpoints)
+    {
+        this.transform.SetParent(containerTransform);
+        this.sr.sprite = sprite;
+        this.sr.color = color;
+        this.HitPoints = hitpoints;
+    }
+}
---
+++ Assets/Scripts/BrickManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static BrickManager _instance;
+    public static BrickManager Instance => _instance;
+
+    private void Awake() 
+    {
+        if(!_instance == null )
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+    private int maxRows = 17;
+    private int maxCols = 12;
+    private GameObject bricksContainer;
+    private float initialBrickSpawnPositionX = -1.96f;
+    private float initialBrickSpawnPositionY = 3.325f;
+    public Brick brickPrefab;
+    public Sprite[] Sprites;
+    public Color[] BrickColors;
+    private float shiftAmount = 0.365f;
+    public int InitialBricksCount{get; set;}
+    public List<Brick> RemainingBricks{ get; set;} 
+    public List<int[,]> LevelsData { get; set;}
+
+    public int CurrentLevel;
+    private void Start() 
+    {
+        this.bricksContainer = new GameObject("BricksContainer");
+        this.RemainingBricks = new List<Brick>();
+        this.LevelsData = this.LoadLevelsData();
+        this.GenerateBricks();
+        
+    }
+
+    private void GenerateBricks()
+    {
+        int[,] currentLevelData = this.LevelsData[this.CurrentLevel];
+        float currentSpawnX = initialBrickSpawnPositionX;
+        float currentSpawnY = initialBrickSpawnPositionY; 
+        float zShift = 0f;
+
+        for (int row = 0; row < this.maxRows; row++)
+        {
+            for (int col = 0; col < this.maxCols; col++)
+            {
+                int brickType = currentLevelData[row, col];
+
+                if(brickType > 0)
+                {
+                    Brick newBrick = Instantiate(brickPrefab, new Vector3(currentSpawnX, currentSpawnY, 0.0f - zShift ), Quaternion.identity) as Brick;
+                    newBrick.Init(bricksContainer.transform, this.Sprites[brickType - 1], this.BrickColors[brickType], brickType);
+
+                    this.RemainingBricks.Add(newBrick);
+                    zShift += 0.0001f;
+                }
+                currentSpawnX += shiftAmount;
+                if(col + 1 == this.maxCols)
+                {
+                    currentSpawnX = initialBrickSpawnPositionX;
+                }
+            }
+            currentSpawnY -= shiftAmount;
+        }
+        this.InitialBricksCount = this.RemainingBricks.Count;
+    }
+    private List<int[,]> LoadLevelsData()
+    {
+        TextAsset text = Resources.Load("Levels") as TextAsset;
+
+        string[] rows = text.text.Split(new string[] { Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+
+        List<int[,]> levelData = new List<int[,]>();
+        int[,] currentLevel = new int[maxRows, maxCols];
+        
+        int currentRow = 0;
+
+        for(int row = 0; row < rows.Length ; row++)
+        {
+            string line = rows[row];
+
+            if(line.IndexOf("--") == -1)
+            {
+                string[] bricks = line.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0 ; col <bricks.Length ; col++)
+                {
+                    currentLevel[currentRow, col] = int.Parse(bricks[col]);
+                }
+                currentRow++;
+            }
+            else
+            {
+                //end of current level
+                //add the matrix to the last and continue the loop
+                currentRow = 0;
+                levelData.Add(currentLevel);
+                currentLevel = new int[maxRows, maxCols];
+            }
+        }
+        return levelData;
+    }
+}
